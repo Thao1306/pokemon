58383275c5a769e6c3353572b65623f3c35d290a
lephuongthao1306@gmail.com <lephuongthao1306@gmail.com>
2022-05-25 13:39:35 +0700

    fix url github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import Detail from './pages/Detail';
-import Home from './pages/Home';
-import './style/App.css';
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-import NoMatch from './pages/NoMatch';
-
+import React from "react";
+import Detail from "./pages/Detail";
+import Home from "./pages/Home";
+import "./style/App.css";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Link,
+  Route,
+  Routes
+} from "react-router-dom";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
-  return <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="detail/:id" element={<Detail />} />
-      <Route path="*" element={<NoMatch />} />
-    </Routes>
-  </Router>
-
-
-
-
+  return (
+    <HashRouter basename="/pokemon">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="detail/:id" element={<Detail />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
